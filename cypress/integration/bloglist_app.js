f67cdee95a01d8114c9eb2fdc0b4a864c812b485
Vilhelm Toivonen <vilhelm.toivonen@gmail.com>
2022-01-11 23:27:15 +0200

    Fixed localhost port to 5k

--- cypress/integration/bloglist_app.js
+++ cypress/integration/bloglist_app.js
@@ -1,6 +1,6 @@
 describe('Bloglist', function() {
     it('front page can be opened', function() {
-        cy.visit('http://localhost:3001')
+        cy.visit('http://localhost:5000')
         cy.contains('Phonebook')
         cy.contains('add a new')
         cy.contains('Numbers')
