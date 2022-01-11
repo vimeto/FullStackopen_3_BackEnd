3358f13e976a68025ab1dea814e9c6bd74fffcef
Vilhelm Toivonen <vilhelm.toivonen@gmail.com>
2022-01-11 22:16:22 +0200

    Changed dotenv variables to github secrets and JS variabels

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+// require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
@@ -115,7 +115,7 @@ app.use(unknownEndpoint)
 app.use(errorHandler)
 
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
